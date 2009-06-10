4f967cab17ff640a453840f1b9078aa5c2e05ca5
Kristaps Dzonsons <kristaps@bsd.lv>
2009-06-10 20:18:43 +0000

    Fixed license email address.

--- man_validate.c
+++ man_validate.c
@@ -1,6 +1,6 @@
 /*	$Id$ */
 /*
- * Copyright (c) 2008, 2009 Kristaps Dzonsons <kristaps@openbsd.org>
+ * Copyright (c) 2008, 2009 Kristaps Dzonsons <kristaps@kth.se>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
