e72ec33b57dbd8394c4748b81786beae018d4bf9
Kristaps Dzonsons <kristaps@bsd.lv>
2009-07-12 08:49:50 +0000

    Noted dissent with \- for OpenBSD.

--- mdoc_term.c
+++ mdoc_term.c
@@ -1134,7 +1134,11 @@ termp_ex_pre(DECL_ARGS)
 static int
 termp_nd_pre(DECL_ARGS)
 {
-
+	/* 
+	 * XXX: signed off by jmc@openbsd.org.  This technically
+	 * produces a minus sign after the Nd, which is wrong, but is
+	 * consistent with the historic OpenBSD tmac file.
+	 */
 #ifdef __OpenBSD__
 	term_word(p, "\\-");
 #else
