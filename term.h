3f4c62fdd734e768f0c3ee2fea0217eed98ba2ba
Kristaps Dzonsons <kristaps@bsd.lv>
2009-07-24 12:47:35 +0000

    Full support for `An -split/-nosplit'.  Compat documented.

--- term.h
+++ term.h
@@ -48,6 +48,9 @@ struct	termp {
 #define	TERMP_DANGLE	 (1 << 8)	/* See term_flushln(). */
 #define	TERMP_HANG	 (1 << 9)	/* See term_flushln(). */
 #define	TERMP_TWOSPACE	 (1 << 10)	/* See term_flushln(). */
+#define	TERMP_NOSPLIT	 (1 << 11)	/* See termp_an_pre/post(). */
+#define	TERMP_SPLIT	 (1 << 12)	/* See termp_an_pre/post(). */
+#define	TERMP_ANPREC	 (1 << 13)	/* See termp_an_pre(). */
 	char		 *buf;		/* Output buffer. */
 	enum termenc	  enc;		/* Type of encoding. */
 	void		 *symtab;	/* Encoded-symbol table. */
