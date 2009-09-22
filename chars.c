2d644b8c1b43567facbded3b0665db8b0c5e8e46
Kristaps Dzonsons <kristaps@bsd.lv>
2009-09-22 10:07:01 +0000

    Many more characters for HMTL (mostly Greek).

--- chars.c
+++ chars.c
@@ -38,7 +38,7 @@ struct	ln {
 #define CHARS_BOTH	 (0x03)
 };
 
-#define	LINES_MAX	  266
+#define	LINES_MAX	  267
 
 #define CHAR(w, x, y, z, a, b) \
 	{ NULL, (w), (y), (a), (x), (z), (b), CHARS_CHAR },
