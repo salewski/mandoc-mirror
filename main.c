1eb06d331eed98461445f2f9397b37eb8db88a93
Kristaps Dzonsons <kristaps@bsd.lv>
2009-10-26 15:44:51 +0000

    Merged patch to allow -fign-escape (thanks Joerg Sonnenberger).

--- main.c
+++ main.c
@@ -571,7 +571,7 @@ static int
 foptions(int *fflags, char *arg)
 {
 	char		*v, *o;
-	const char	*toks[7];
+	const char	*toks[8];
 
 	toks[0] = "ign-scope";
 	toks[1] = "no-ign-escape";
@@ -579,7 +579,8 @@ foptions(int *fflags, char *arg)
 	toks[3] = "no-ign-chars";
 	toks[4] = "ign-errors";
 	toks[5] = "strict";
-	toks[6] = NULL;
+	toks[6] = "ign-escape";
+	toks[7] = NULL;
 
 	while (*arg) {
 		o = arg;
@@ -602,6 +603,9 @@ foptions(int *fflags, char *arg)
 		case (5):
 			*fflags |= NO_IGN_ESCAPE | 
 			 	   NO_IGN_MACRO | NO_IGN_CHARS;
+			break;
+		case (6):
+			*fflags &= ~NO_IGN_ESCAPE;
 			break;
 		default:
 			warnx("bad argument: -f%s", o);
