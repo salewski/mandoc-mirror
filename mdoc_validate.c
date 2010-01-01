8fa3cab54045799dd99bbcaf29bca6e52e2a5e79
Kristaps Dzonsons <kristaps@bsd.lv>
2010-01-01 15:14:03 +0000

    `Bl' lets `Sm' slip by as a child.  Noted by Ingo Schwarze.

--- mdoc_validate.c
+++ mdoc_validate.c
@@ -1086,11 +1086,18 @@ post_bl(POST_ARGS)
 	if (NULL == mdoc->last->child)
 		return(1);
 
+	/*
+	 * We only allow certain children of `Bl'.  This is usually on
+	 * `It', but apparently `Sm' occurs here and there, so we let
+	 * that one through, too.
+	 */
+
 	/* LINTED */
 	for (n = mdoc->last->child; n; n = n->next) {
-		if (MDOC_BLOCK == n->type) 
-			if (MDOC_It == n->tok)
-				continue;
+		if (MDOC_BLOCK == n->type && MDOC_It == n->tok)
+			continue;
+		if (MDOC_Sm == n->tok)
+			continue;
 		return(mdoc_nerr(mdoc, n, EBADCHILD));
 	}
 
