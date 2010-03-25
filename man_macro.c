0a6ccaccd6faa14d500d23d3caefbc53e8bcf563
Kristaps Dzonsons <kristaps@bsd.lv>
2010-03-25 07:39:25 +0000

    Lint fixes.

--- man_macro.c
+++ man_macro.c
@@ -222,6 +222,7 @@ rew_scope(enum man_type type, struct man *m, enum mant tok)
  * any of these as the parent node, so it needs special handling.
  * Beyond this, it's the same as blk_close().
  */
+/* ARGSUSED */
 int
 blk_dotted(MACRO_PROT_ARGS)
 {
@@ -255,6 +256,7 @@ blk_dotted(MACRO_PROT_ARGS)
 /*
  * Close out a generic explicit macro.
  */
+/* ARGSUSED */
 int
 blk_close(MACRO_PROT_ARGS)
 {
