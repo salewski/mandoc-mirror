c1ff47bfc1000db24f197f7d5ed017d23745ce0b
Kristaps Dzonsons <kristaps@bsd.lv>
2010-12-06 16:55:35 +0000

    Bring schwarze@'s mandoc.h and main.c errors and warnings entirely in sync, except for the tbl one which will be the focus of later study.

--- mandoc.h
+++ mandoc.h
@@ -45,72 +45,84 @@ enum	mandocerr {
 	MANDOCERR_OK,
 
 	MANDOCERR_WARNING, /* ===== start of warnings ===== */
-	MANDOCERR_SO, /* .so is fragile, better use ln(1) */
-	MANDOCERR_UPPERCASE, /* text should be uppercase */
-	MANDOCERR_SECOOO, /* sections out of conventional order */
-	MANDOCERR_SECREP, /* section name repeats */
-	MANDOCERR_PROLOGOOO, /* out of order prologue */
-	MANDOCERR_PROLOGREP, /* repeated prologue entry */
-	MANDOCERR_LISTFIRST, /* list type must come first */
-	MANDOCERR_BADTAB, /* tab in non-literal context */
-	MANDOCERR_BADESCAPE, /* bad escape sequence */
-	MANDOCERR_BADQUOTE, /* unterminated quoted string */
-	MANDOCERR_NOWIDTHARG, /* argument requires the width argument */
-	MANDOCERR_BADDATE, /* bad date argument */
-	MANDOCERR_BADWIDTH, /* bad width argument */
-	MANDOCERR_BADMSEC, /* unknown manual section */
-	MANDOCERR_SECMSEC, /* section not in conventional manual section */
-	MANDOCERR_EOLNSPACE, /* end of line whitespace */
-	MANDOCERR_SCOPENEST, /* blocks badly nested */
 
-	MANDOCERR_ERROR, /* ===== start of errors ===== */
+	/* related to the prologue */
+	MANDOCERR_NOTITLE, /* no title in document */
+	MANDOCERR_UPPERCASE, /* document title should be all caps */
+	MANDOCERR_BADMSEC, /* unknown manual section */
+	MANDOCERR_BADDATE, /* cannot parse date argument */
+	MANDOCERR_PROLOGOOO, /* prologue macros out of order */
+	MANDOCERR_PROLOGREP, /* duplicate prologue macro */
+	MANDOCERR_BADPROLOG, /* macro not allowed in prologue */
+	MANDOCERR_BADBODY, /* macro not allowed in body */
+
+	/* related to document structure */
+	MANDOCERR_SO, /* .so is fragile, better use ln(1) */
 	MANDOCERR_NAMESECFIRST, /* NAME section must come first */
+	MANDOCERR_BADNAMESEC, /* bad NAME section contents */
+	MANDOCERR_NONAME, /* manual name not yet set */
+	MANDOCERR_SECOOO, /* sections out of conventional order */
+	MANDOCERR_SECREP, /* duplicate section name */
+	MANDOCERR_SECMSEC, /* section not in conventional manual section */
+
+	/* related to macros and nesting */
+	MANDOCERR_MACROOBS, /* skipping obsolete macro */
+	MANDOCERR_IGNPAR, /* skipping paragraph macro */
+	MANDOCERR_SCOPENEST, /* blocks badly nested */
+	MANDOCERR_CHILD, /* child violates parent syntax */
+	MANDOCERR_NESTEDDISP, /* nested displays are not portable */
+	MANDOCERR_SCOPEREP, /* already in literal mode */
+
+	/* related to missing macro arguments */
+	MANDOCERR_MACROEMPTY, /* skipping empty macro */
+	MANDOCERR_DISPTYPE, /* missing display type */
+	MANDOCERR_LISTFIRST, /* list type must come first */
+	MANDOCERR_NOWIDTHARG, /* tag lists require a width argument */
+	MANDOCERR_FONTTYPE, /* missing font type */
+
+	/* related to bad macro arguments */
+	MANDOCERR_IGNARGV, /* skipping argument */
+	MANDOCERR_ARGVREP, /* duplicate argument */
+	MANDOCERR_DISPREP, /* duplicate display type */
+	MANDOCERR_LISTREP, /* duplicate list type */
+	MANDOCERR_BADATT, /* unknown AT&T UNIX version */
 	MANDOCERR_BADBOOL, /* bad Boolean value */
 	MANDOCERR_BADFONT, /* unknown font */
-	MANDOCERR_CHILD, /* child violates parent syntax */
-	MANDOCERR_NESTEDDISP, /* displays may not be nested */
-	MANDOCERR_BADATT, /* bad AT&T symbol */
-	MANDOCERR_BADSTANDARD, /* bad standard */
-	MANDOCERR_LISTREP, /* list type repeated */
-	MANDOCERR_DISPREP, /* display type repeated */
-	MANDOCERR_ARGVREP, /* argument repeated */
-	MANDOCERR_IGNARGV, /* ignoring argument */
-	MANDOCERR_NONAME, /* manual name not yet set */
-	MANDOCERR_MACROOBS, /* obsolete macro ignored */
-	MANDOCERR_MACROEMPTY, /* empty macro ignored */
-	MANDOCERR_BADBODY, /* macro not allowed in body */
-	MANDOCERR_BADPROLOG, /* macro not allowed in prologue */
-	MANDOCERR_BADCHAR, /* bad character */
-	MANDOCERR_BADNAMESEC, /* bad NAME section contents */
-	MANDOCERR_NOBLANKLN, /* no blank lines */
-	MANDOCERR_NOTEXT, /* no text in this context */
+	MANDOCERR_BADSTANDARD, /* unknown standard specifier */
+	MANDOCERR_BADWIDTH, /* bad width argument */
+
+	/* related to plain text */
+	MANDOCERR_NOBLANKLN, /* blank line in non-literal context */
+	MANDOCERR_BADTAB, /* tab in non-literal context */
+	MANDOCERR_EOLNSPACE, /* end of line whitespace */
 	MANDOCERR_BADCOMMENT, /* bad comment style */
-	MANDOCERR_MACRO, /* unknown macro will be lost */
+	MANDOCERR_BADESCAPE, /* unknown escape sequence */
+	MANDOCERR_BADQUOTE, /* unterminated quoted string */
+
+	MANDOCERR_ERROR, /* ===== start of errors ===== */
+
+	MANDOCERR_BADCHAR, /* skipping bad character */
+	MANDOCERR_NOTEXT, /* skipping text before the first section header */
+	MANDOCERR_MACRO, /* skipping unknown macro */
 	MANDOCERR_REQUEST, /* NOT IMPLEMENTED: skipping request */
 	MANDOCERR_LINESCOPE, /* line scope broken */
 	MANDOCERR_ARGCOUNT, /* argument count wrong */
-	MANDOCERR_NOSCOPE, /* no such block is open */
+	MANDOCERR_NOSCOPE, /* skipping end of block that is not open */
 	MANDOCERR_SCOPEBROKEN, /* missing end of block */
-	MANDOCERR_SCOPEREP, /* scope already open */
 	MANDOCERR_SCOPEEXIT, /* scope open on exit */
 	MANDOCERR_UNAME, /* uname(3) system call failed */
 	/* FIXME: merge following with MANDOCERR_ARGCOUNT */
 	MANDOCERR_NOARGS, /* macro requires line argument(s) */
 	MANDOCERR_NOBODY, /* macro requires body argument(s) */
 	MANDOCERR_NOARGV, /* macro requires argument(s) */
-	MANDOCERR_NOTITLE, /* no title in document */
 	MANDOCERR_LISTTYPE, /* missing list type */
-	MANDOCERR_DISPTYPE, /* missing display type */
-	MANDOCERR_FONTTYPE, /* missing font type */
 	MANDOCERR_ARGSLOST, /* line argument(s) will be lost */
 	MANDOCERR_BODYLOST, /* body argument(s) will be lost */
-	MANDOCERR_IGNPAR, /* paragraph macro ignored */
 
 	MANDOCERR_FATAL, /* ===== start of fatal errors ===== */
+
 	MANDOCERR_COLUMNS, /* column syntax is inconsistent */
-	MANDOCERR_BADDISP, /* unsupported display type */
-	MANDOCERR_SCOPEFATAL, /* blocks badly nested */
-	MANDOCERR_SYNTNOSCOPE, /* no scope to rewind: syntax violated */
+	MANDOCERR_BADDISP, /* NOT IMPLEMENTED: .Bd -file */
 	MANDOCERR_SYNTLINESCOPE, /* line scope broken, syntax violated */
 	MANDOCERR_SYNTARGVCOUNT, /* argument count wrong, violates syntax */
 	MANDOCERR_SYNTCHILD, /* child violates parent syntax */
