453c46e288d7d413409344d7389499be88b36229
Kristaps Dzonsons <kristaps@bsd.lv>
2010-12-17 12:57:49 +0000

    Don't use EM for default widths; use BU instead.

--- mdoc_html.c
+++ mdoc_html.c
@@ -287,7 +287,7 @@ a2width(const char *p, struct roffsu *su)
 {
 
 	if ( ! a2roffsu(p, su, SCALE_MAX)) {
-		su->unit = SCALE_EM;
+		su->unit = SCALE_BU;
 		su->scale = (int)strlen(p);
 	}
 }
@@ -354,7 +354,7 @@ a2offs(const char *p, struct roffsu *su)
 	else if (0 == strcmp(p, "indent-two"))
 		SCALE_HS_INIT(su, INDENT * 2);
 	else if ( ! a2roffsu(p, su, SCALE_MAX)) {
-		su->unit = SCALE_EM;
+		su->unit = SCALE_BU;
 		su->scale = (int)strlen(p);
 	}
 }
