ae433dddf1c1ce720888c94ae8b549dbb9fa0dc6
Ingo Schwarze <schwarze@mandoc.bsd.lv>
2011-01-03 22:42:37 +0000

    Unify roff macro argument parsing (in roff.c, roff_userdef()) and man macro argument parsing (in man_argv.c, man_args()), both having different bugs, to use one common macro argument parser (in mandoc.c, mandoc_getarg()), because from the point of view of roff, man macros are just roff macros, hence their arguments are parsed in exactly the same way.
    While doing so, fix these bugs:
     * Escaped blanks (i.e. those preceded by an odd number of backslashes)
       were mishandled as argument separators in unquoted arguments to
       user-defined roff macros.
     * Unescaped blanks preceded by an even number of backslashes were not
       recognized as argument separators in unquoted arguments to man macros.
     * Escaped backslashes (i.e. pairs of backslashes) were not reduced
       to single backslashes both in unquoted and quoted arguments both
       to user-defined roff macros and to man macros.
     * Escaped quotes (i.e. pairs of quotes inside quoted arguments) were
       not reduced to single quotes in man macros.
    
    OK kristaps@
    
    Note that mdoc macro argument parsing is yet another beast for no good
    reason and is probably afflicted by similar bugs.  But i don't attempt
    to fix that right now because it is intricately entangled with lots of
    unrelated high-level mdoc(7) functionality, like delimiter handling and
    column list phrase handling.  Disentagling that would waste too much
    time now.

--- libmandoc.h
+++ libmandoc.h
@@ -24,6 +24,7 @@ void		*mandoc_calloc(size_t, size_t);
 char		*mandoc_strdup(const char *);
 void		*mandoc_malloc(size_t);
 void		*mandoc_realloc(void *, size_t);
+char		*mandoc_getarg(char **, mandocmsg, void *, int, int *);
 time_t		 mandoc_a2time(int, const char *);
 #define		 MTIME_CANONICAL	(1 << 0)
 #define		 MTIME_REDUCED		(1 << 1)
