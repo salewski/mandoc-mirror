09bb65ecd786da147b05a784dcae445ff3d3e37f
Kristaps Dzonsons <kristaps@bsd.lv>
2011-07-22 00:16:37 +0000

    Accomodate for hard-spaces with tildes.  For now, consider them regular spaces.  Also allow for tabs.  Finally, have the parser correctly handle open and close brackets smooshed against other terms.  All of these handle "details" noted in the CACM paper.

--- eqn.c
+++ eqn.c
@@ -453,11 +453,11 @@ eqn_next(struct eqn_node *ep, char quote, size_t *sz, int repl)
 {
 	char		*start, *next;
 	int		 q, diff, lim;
-	size_t		 ssz;
+	size_t		 ssz, dummy;
 	struct eqn_def	*def;
 
 	if (NULL == sz)
-		sz = &ssz;
+		sz = &dummy;
 
 	lim = 0;
 	ep->rew = ep->cur;
@@ -482,14 +482,26 @@ again:
 	}
 
 	start = &ep->data[(int)ep->cur];
-	next = q ? strchr(start, quote) : strchr(start, ' ');
+
+	if ( ! q) {
+		if ('{' == *start || '}' == *start)
+			ssz = 1;
+		else
+			ssz = strcspn(start + 1, " ~\"{}\t") + 1;
+		next = start + (int)ssz;
+		if ('\0' == *next)
+			next = NULL;
+	} else
+		next = strchr(start, quote);
 
 	if (NULL != next) {
 		*sz = (size_t)(next - start);
 		ep->cur += *sz;
 		if (q)
 			ep->cur++;
-		while (' ' == ep->data[(int)ep->cur])
+		while (' ' == ep->data[(int)ep->cur] ||
+				'\t' == ep->data[(int)ep->cur] ||
+				'~' == ep->data[(int)ep->cur])
 			ep->cur++;
 	} else {
 		if (q)
