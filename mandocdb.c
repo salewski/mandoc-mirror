3fe60eddb41d91b0a568a4213b061a4f20347fb1
Ingo Schwarze <schwarze@mandoc.bsd.lv>
2013-12-31 19:40:20 +0000

    Yet another regression introduced by Kristaps when he switched from Berkeley DB to SQLite3: In the .In parser, the logic got inverted. The resulting NULL pointer access was found by clang; scan log provided by Ulrich Spoerlein <uqs at FreeBSD>.
    The best fix is to simply remove the whole, pointless custom
    handler function for .In and let the framework do its work.
    Now searching for included header files actually works.
    
    While here, remove the similarly pointless custom .St handler,
    fix the return value of the .Fd handler and disentangle the
    spaghetti in the .Nm handler.

--- mandocdb.c
+++ mandocdb.c
@@ -154,11 +154,9 @@ static	int	 parse_mdoc_body(struct mpage *, const struct mdoc_node *);
 static	int	 parse_mdoc_head(struct mpage *, const struct mdoc_node *);
 static	int	 parse_mdoc_Fd(struct mpage *, const struct mdoc_node *);
 static	int	 parse_mdoc_Fn(struct mpage *, const struct mdoc_node *);
-static	int	 parse_mdoc_In(struct mpage *, const struct mdoc_node *);
 static	int	 parse_mdoc_Nd(struct mpage *, const struct mdoc_node *);
 static	int	 parse_mdoc_Nm(struct mpage *, const struct mdoc_node *);
 static	int	 parse_mdoc_Sh(struct mpage *, const struct mdoc_node *);
-static	int	 parse_mdoc_St(struct mpage *, const struct mdoc_node *);
 static	int	 parse_mdoc_Xr(struct mpage *, const struct mdoc_node *);
 static	void	 putkey(const struct mpage *,
 			const char *, uint64_t);
@@ -216,7 +214,7 @@ static	const struct mdoc_handler mdocs[MDOC_MAX] = {
 	{ parse_mdoc_Fn, 0 },  /* Fn */
 	{ NULL, TYPE_Ft },  /* Ft */
 	{ NULL, TYPE_Ic },  /* Ic */
-	{ parse_mdoc_In, TYPE_In },  /* In */
+	{ NULL, TYPE_In },  /* In */
 	{ NULL, TYPE_Li },  /* Li */
 	{ parse_mdoc_Nd, TYPE_Nd },  /* Nd */
 	{ parse_mdoc_Nm, TYPE_Nm },  /* Nm */
@@ -224,7 +222,7 @@ static	const struct mdoc_handler mdocs[MDOC_MAX] = {
 	{ NULL, 0 },  /* Ot */
 	{ NULL, TYPE_Pa },  /* Pa */
 	{ NULL, 0 },  /* Rv */
-	{ parse_mdoc_St, 0 },  /* St */
+	{ NULL, TYPE_St },  /* St */
 	{ NULL, TYPE_Va },  /* Va */
 	{ parse_mdoc_body, TYPE_Va },  /* Vt */
 	{ parse_mdoc_Xr, 0 },  /* Xr */
@@ -1441,18 +1439,7 @@ parse_mdoc_Fd(struct mpage *mpage, const struct mdoc_node *n)
 
 	if (end > start)
 		putkeys(mpage, start, end - start + 1, TYPE_In);
-	return(1);
-}
-
-static int
-parse_mdoc_In(struct mpage *mpage, const struct mdoc_node *n)
-{
-
-	if (NULL != n->child && MDOC_TEXT == n->child->type)
-		return(0);
-
-	putkey(mpage, n->child->string, TYPE_In);
-	return(1);
+	return(0);
 }
 
 static int
@@ -1486,17 +1473,6 @@ parse_mdoc_Fn(struct mpage *mpage, const struct mdoc_node *n)
 			putkey(mpage, n->string, TYPE_Fa);
 
 	return(0);
-}
-
-static int
-parse_mdoc_St(struct mpage *mpage, const struct mdoc_node *n)
-{
-
-	if (NULL == n->child || MDOC_TEXT != n->child->type)
-		return(0);
-
-	putkey(mpage, n->child->string, TYPE_St);
-	return(1);
 }
 
 static int
@@ -1556,12 +1532,8 @@ static int
 parse_mdoc_Nm(struct mpage *mpage, const struct mdoc_node *n)
 {
 
-	if (SEC_NAME == n->sec)
-		return(1);
-	else if (SEC_SYNOPSIS != n->sec || MDOC_HEAD != n->type)
-		return(0);
-
-	return(1);
+	return(SEC_NAME == n->sec ||
+	    (SEC_SYNOPSIS == n->sec && MDOC_HEAD == n->type));
 }
 
 static int
