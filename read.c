c2ffdeee9a284888d14cc25d8a8b96f635150688
Ingo Schwarze <schwarze@mandoc.bsd.lv>
2016-12-07 22:59:29 +0000

    When reporting "whitespace at end of input line" on lines ending with roff(7) comments, let the column number in the message point to the end of the line rather than to the beginning of the comment. Improvement suggested by bluhm@.

--- read.c
+++ read.c
@@ -315,6 +315,7 @@ mparse_buf_r(struct mparse *curp, struct buf blk, size_t i, int start)
 	const char	*save_file;
 	char		*cp;
 	size_t		 pos; /* byte number in the ln buffer */
+	size_t		 j;  /* auxiliary byte number in the blk buffer */
 	enum rofferr	 rr;
 	int		 of;
 	int		 lnn; /* line number in the real file */
@@ -420,6 +421,7 @@ mparse_buf_r(struct mparse *curp, struct buf blk, size_t i, int start)
 			}
 
 			if ('"' == blk.buf[i + 1] || '#' == blk.buf[i + 1]) {
+				j = i;
 				i += 2;
 				/* Comment, skip to end of line */
 				for (; i < blk.sz; ++i) {
@@ -430,7 +432,7 @@ mparse_buf_r(struct mparse *curp, struct buf blk, size_t i, int start)
 						mandoc_msg(
 						    MANDOCERR_SPACE_EOL,
 						    curp, curp->line,
-						    pos, NULL);
+						    pos + i-1 - j, NULL);
 					++i;
 					++lnn;
 					break;
