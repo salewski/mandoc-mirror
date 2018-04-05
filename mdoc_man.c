decb22f81bccd7cc79f60b2ead87ac3bae2c34e3
Ingo Schwarze <schwarze@mandoc.bsd.lv>
2018-04-05 08:47:16 +0000

    For .Do/.Dq, use the documented and portable \(lq and \(rq character escape sequences rather than the undocumented and non-portable \(Lq and \(Rq.
    Bug reported by Tim L <darkxst at github>
    via Thomas Klausner <wiz at NetBSD>;
    see https://github.com/nih-at/libzip/pull/42

--- mdoc_man.c
+++ mdoc_man.c
@@ -202,8 +202,8 @@ static	const struct manact __manacts[MDOC_MAX - MDOC_Dd] = {
 	{ NULL, pre_bk, post_bk, NULL, NULL }, /* Bx */
 	{ NULL, pre_skip, NULL, NULL, NULL }, /* Db */
 	{ NULL, NULL, NULL, NULL, NULL }, /* Dc */
-	{ cond_body, pre_enc, post_enc, "\\(Lq", "\\(Rq" }, /* Do */
-	{ cond_body, pre_enc, post_enc, "\\(Lq", "\\(Rq" }, /* Dq */
+	{ cond_body, pre_enc, post_enc, "\\(lq", "\\(rq" }, /* Do */
+	{ cond_body, pre_enc, post_enc, "\\(lq", "\\(rq" }, /* Dq */
 	{ NULL, NULL, NULL, NULL, NULL }, /* Ec */
 	{ NULL, NULL, NULL, NULL, NULL }, /* Ef */
 	{ NULL, pre_em, post_font, NULL, NULL }, /* Em */
