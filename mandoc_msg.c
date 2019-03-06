e4f4c1fcd540441666f7e5a9cbb4f63d1c1f3332
Ingo Schwarze <schwarze@mandoc.bsd.lv>
2019-03-06 15:55:38 +0000

    this file needs to #include "config.h" because it calls getprogname(); found while testing on Debian jessie

--- mandoc_msg.c
+++ mandoc_msg.c
@@ -15,6 +15,8 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
+#include "config.h"
+
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
